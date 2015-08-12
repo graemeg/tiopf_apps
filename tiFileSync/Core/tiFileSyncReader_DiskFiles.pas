9309dc8b648ca910217e7176dddbfb3a27a84238
Graeme Geldenhuys <graemeg@gmail.com>
2015-08-12 09:43:01 +0100

    Replaces Windows file wildcard with a cross-platform constant.

--- tiFileSync/Core/tiFileSyncReader_DiskFiles.pas
+++ tiFileSync/Core/tiFileSyncReader_DiskFiles.pas
@@ -141,7 +141,7 @@ begin
   // Read files to include
   if (pSourceFileNameFilters = nil) or
      (pSourceFileNameFilters.CountNotDeleted = 0) then
-    _IncludeFiles(pFileNames, lStartDir, '*.*')
+    _IncludeFiles(pFileNames, lStartDir, AllFilesWildCard)
   else
   begin
     for i := 0 to pSourceFileNameFilters.Count - 1 do
